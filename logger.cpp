4c4fa114554914a734cb1c9917ba119c2df94f46
Abdulkerim Elmas <elm4s@icloud.com>
2025-10-05 13:16:47 +0300

    Add log macros

--- logger.cpp
+++ logger.cpp
@@ -1,12 +1,20 @@
 #include "logger.h"
 
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
 #include "spdlog/sinks/qt_sinks.h"
 #include "spdlog/spdlog.h"
+
+#define LOGT SPDLOG_TRACE
+#define LOGD SPDLOG_DEBUG
+#define LOGI SPDLOG_INFO
+#define LOGW SPDLOG_WARN
+#define LOGE SPDLOG_ERROR
+#define LOGC SPDLOG_CRITICAL
 
 void Logger::initialize(QTextEdit* logView,
     int maxLines)
 {
     auto logger = spdlog::qt_color_logger_mt("qt_logger", logView, maxLines);
     spdlog::set_default_logger(logger);
-    spdlog::info("new logger log message");
+    LOGI("Logger initialized.");
 }
